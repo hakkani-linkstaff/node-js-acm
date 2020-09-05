01ffc6a70f3c54a0ac12dabcdddd01502bbe212a
Hakkani <refathasan5124@gmail.com>
2020-09-05 13:48:21 +0900

    74. Serving Files Statically

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ const shopRoutes = require("./routes/shop");
 
 // body parser
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
