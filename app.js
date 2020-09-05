89ea9168180d7a0871ef6c5b80290ed0f287c223
Hakkani <refathasan5124@gmail.com>
2020-09-06 00:34:42 +0900

    78. Sharing Data Across Requests & Users

--- app.js
+++ app.js
@@ -24,6 +24,4 @@ app.use((req, res, next) => {
   res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
 });
 
-// const server = http.createServer(app)
-
 app.listen(3000);
--- routes/shop.js
+++ routes/shop.js
@@ -2,13 +2,13 @@ const path = require("path");
 const express = require("express");
 const rootDirectory = require("../util/path");
 
-const route = express.Router();
+const router = express.Router();
 const adminData = require("./admin");
 
-route.get("/", (req, res, next) => {
+router.get("/", (req, res, next) => {
   console.log("[shop.js]", adminData.products);
   res.sendFile(path.join(rootDirectory, "./views", "shop.html"));
 });
 
-// module.exports = route
-module.exports = route;
+// module.exports = router
+module.exports = router;
